4d51192478f0fb565bf9485b780c32b5fe5c1da1
tylor.kolbeck@gmail.com <tylor.kolbeck@gmail.com>
2020-12-03 19:43:12 -0500

    add and remove item functionality added to checkout item component

--- src/components/checkout-item/checkout-item.component.jsx
+++ src/components/checkout-item/checkout-item.component.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import { useDispatch } from 'react-redux'
+import {
+  clearItemFromCart,
+  addItem,
+  removeItemFromCart
+} from '../../redux/cart/cart.actions'
 import './checkout-item.styles.scss'
 
 function CheckoutItem({ cartItem }) {
   const { name, imageUrl, price, quantity } = cartItem
+  const dispatch = useDispatch()
 
   return (
     <div className='checkout-item'>
@@ -10,9 +17,25 @@ function CheckoutItem({ cartItem }) {
         <img src={imageUrl} alt='item' />
       </div>
       <span className='name'>{name}</span>
-      <span className='quantity'>{quantity}</span>
+      <span className='quantity'>
+        <div
+          className='arrow'
+          onClick={() => dispatch(removeItemFromCart(cartItem))}
+        >
+          &#10094;
+        </div>
+        <span className='value'> {quantity}</span>
+        <div className='arrow' onClick={() => dispatch(addItem(cartItem))}>
+          &#10095;
+        </div>
+      </span>
       <span className='price'>{price}</span>
-      <div className='remove-button'>&#10005;</div>
+      <div
+        className='remove-button'
+        onClick={() => dispatch(clearItemFromCart(cartItem))}
+      >
+        &#10005;
+      </div>
     </div>
   )
 }
--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -1,8 +1,9 @@
 /**
  *
- * @param {*} cartItems
- * @param {*} cartItemToAdd
- * @return {array}
+ * Add an item to the cart
+ * @param {array} cartItems
+ * @param {object} cartItemToAdd
+ * @returns {array}
  */
 export const addItemToCart = (cartItemsArray, cartItemToAdd) => {
   const existingCartItem = cartItemsArray.find(
@@ -17,4 +18,39 @@ export const addItemToCart = (cartItemsArray, cartItemToAdd) => {
   }
 
   return [...cartItemsArray, { ...cartItemToAdd, quantity: 1 }]
+}
+
+/**
+ *
+ * Remove all instances of an item from the cart
+ * @param {array} cartItemsArray
+ * @param {object} cartItemToRemove
+ * @returns {array}
+ */
+export const clearItemFromCart = (cartItemsArray, cartItemToClear) => {
+  return cartItemsArray.filter((cartItem) => cartItem.id !== cartItemToClear.id)
+}
+
+/**
+ * Decrease quantity of an item in the cart by removing one
+ * @param {*} cartItemsArray
+ * @param {*} cartItemToRemove
+ */
+export const removeItemFromCart = (cartItemsArray, cartItemToRemove) => {
+  const existingCartItem = cartItemsArray.find(
+    (cartItem) => cartItem.id === cartItemToRemove.id
+  )
+
+  // If there is only one of this item remove it completely(clear) from the cart
+  if (existingCartItem.quantity === 1) {
+    return cartItemsArray.filter(
+      (cartItem) => cartItem.id !== cartItemToRemove.id
+    )
+  }
+
+  return cartItemsArray.map((cartItem) =>
+    cartItem.id === cartItemToRemove.id
+      ? { ...cartItem, quantity: cartItem.quantity - 1 }
+      : cartItem
+  )
 }
